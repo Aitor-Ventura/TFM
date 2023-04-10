0e78f3978bc9d956c550b60ceb48fcf0576248f4
Aitor Ventura <phsykox@gmail.com>
2023-04-10 19:37:06 +0100

    feat: anims and hud

--- Assets/_Project/Scripts/CharacterController2D.cs
+++ Assets/_Project/Scripts/CharacterController2D.cs
@@ -12,12 +12,15 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private float speed;
     
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
     private Vector2 _motionVector;
+    private bool _isMoving;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _rigidbody.gravityScale = 0;
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -29,7 +32,16 @@ public class CharacterController2D : MonoBehaviour
         if (horizontal != 0 || vertical != 0)
         {
             lastMotionVector = new Vector2(horizontal, vertical).normalized;
+            
+            _animator.SetFloat("lastHorizontal", horizontal);
+            _animator.SetFloat("lastVertical", vertical);
         }
+        
+        _animator.SetFloat("horizontal", horizontal);
+        _animator.SetFloat("vertical", vertical);
+        
+        _isMoving = horizontal != 0 || vertical != 0;
+        _animator.SetBool("isMoving", _isMoving);
     }
 
     private void FixedUpdate()
